8d6d47e1144cd870ef8779628d5df93220d9e29c
José Mora <josemorar96@gmail.com>
2018-05-01 15:29:21 -0600

    Agregada funcion para split de set de datos en training y test, se modifican las funciones de redes neuronales para usar el set de validacion

--- tec/ic/ia/p1/g05.py
+++ tec/ic/ia/p1/g05.py
@@ -7,77 +7,122 @@ from keras.layers import Dense
 import keras
 from keras.optimizers import SGD
 
-
-def red_neuronal_r1_r2(muestra, num_capas, unidades_por_capa, activacion):
+def split_muestra(muestra, porcentaje):
+    cantidad_test = int(len(muestra) * (porcentaje / 100))
+    cantidad_training = len(muestra) - cantidad_test
+    test_set = []
+    training_set = []
+    for i in range(0, len(muestra)):
+        if(i < cantidad_training):
+            training_set += [muestra[i]]
+        else:
+            test_set += [muestra[i]]
+    return training_set, test_set
+        
+def red_neuronal_r1_r2(muestra, test, num_capas, unidades_por_capa, activacion):
     x_train = []
+    x_test = []
+    y_test = get_column(test,-1)
     y_train = get_column(muestra,-1)
     for i in range(0,len(y_train)):
         y_train[i] -= 1
+    for i in range(0,len(y_test)):
+        y_test[i] -= 1
     y_train = keras.utils.to_categorical(y_train,num_classes = 4)
+    y_test = keras.utils.to_categorical(y_test,num_classes = 4)
     for i in range (0, len(muestra)):
         x_train += [muestra[i][:-1]]
+    for i in range (0, len(test)):
+        x_test += [test[i][:-1]]    
     modelo = Sequential()
     modelo.add(Dense(units=unidades_por_capa[0],activation='relu',input_dim=23))
     for i in range(1,num_capas):
         modelo.add(Dense(units=unidades_por_capa[i],activation=activacion))
-    modelo.add(Dense(4,activation='sigmoid'))
+    modelo.add(Dense(4,activation='softmax'))
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     modelo.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
     x_train = np.array(x_train)
     y_train = np.array(y_train)
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
     modelo.fit(x_train, y_train,
                epochs = 20,
-               batch_size = 128)
+               batch_size = 128,
+               validation_data =(x_test,y_test),
+               shuffle = True)
     return modelo
 
-def red_neuronal_r2(muestra, num_capas, unidades_por_capa, activacion):
+def red_neuronal_r2(muestra, test, num_capas, unidades_por_capa, activacion):
     x_train = []
     y_train = get_column(muestra,-1)
+    x_test = []
+    y_test = get_column(test,-1)
+    
     for i in range(0,len(y_train)):
         y_train[i] -= 1
+    for i in range(0,len(y_test)):
+        y_test[i] -= 1
+    y_test = keras.utils.to_categorical(y_test,num_classes = 4)
     y_train = keras.utils.to_categorical(y_train,num_classes = 4)
     for i in range (0, len(muestra)):
         x_train += [muestra[i][:-1]]
+    for i in range (0, len(test)):
+        x_test += [test[i][:-1]] 
     modelo = Sequential()
     modelo.add(Dense(units=unidades_por_capa[0],activation='relu',input_dim=22))
     for i in range(1,num_capas):
         modelo.add(Dense(units=unidades_por_capa[i],activation=activacion))
-    modelo.add(Dense(4,activation='sigmoid'))
+    modelo.add(Dense(4,activation='softmax'))
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     modelo.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
     x_train = np.array(x_train)
     y_train = np.array(y_train)
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
     modelo.fit(x_train, y_train,
                epochs = 20,
-               batch_size = 128)
+               batch_size = 128,
+               validation_data = (x_test, y_test)
+               )
     return modelo
 
-def red_neuronal_r1(muestra, num_capas, unidades_por_capa, activacion):
+def red_neuronal_r1(muestra, test, num_capas, unidades_por_capa, activacion):
     x_train = []
     y_train = get_column(muestra,-1)
+    x_test = []
+    y_test = get_column(test,-1)
     for i in range(0,len(y_train)):
         y_train[i] -= 1
+    for i in range(0,len(y_test)):
+        y_test[i] -= 1
     y_train = keras.utils.to_categorical(y_train,num_classes = 15)
+    y_test = keras.utils.to_categorical(y_test,num_classes = 15)
     for i in range (0, len(muestra)):
         x_train += [muestra[i][:-1]]
+    for i in range (0, len(test)):
+        x_test += [test[i][:-1]] 
     modelo = Sequential()
     modelo.add(Dense(units=unidades_por_capa[0],activation='relu',input_dim=22))
     for i in range(1,num_capas):
         modelo.add(Dense(units=unidades_por_capa[i],activation=activacion))
-    modelo.add(Dense(15,activation='sigmoid'))
+    modelo.add(Dense(15,activation='softmax'))
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     modelo.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
     x_train = np.array(x_train)
     y_train = np.array(y_train)
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
     modelo.fit(x_train, y_train,
                epochs = 20,
-               batch_size = 128)
+               batch_size = 128,
+               validation_data =(x_test,y_test),
+               shuffle = True)
     return modelo
     
     
